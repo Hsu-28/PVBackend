3414617bab33fcc4468fb72583488ac1f225f7c2
Hsu-28 <svm3g6m4s@gmail.com>
2023-09-22 16:30:55 +0800

    0

---
+++ PVBackendFake/src/router/index.js
@@ -0,0 +1,81 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/Donate',
+    name: '/Donate',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Donate.vue')
+  },
+  {
+    path: '/Administrator',
+    name: 'Administrator',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Administrator.vue')
+  },
+  {
+    path: '/FEcontent',
+    name: 'FEcontent',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/FEcontent.vue')
+  },
+  {
+    path: '/Robot',
+    name: 'Robot',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Robot.vue')
+  },
+  {
+    path: '/Process',
+    name: 'Process',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Process.vue')
+  },
+  {
+    path: '/Order',
+    name: 'Order',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Order.vue')
+  },
+  {
+    path: '/Group',
+    name: 'Group',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Group.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ PVBackendFake/vue.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  filenameHashing:false,
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+
+          @import "~@/assets/sass/style.scss";
+          
+        `
+      },
+    }
+  },
+  
+    transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/chd103/g3/planet_voyager/'
+    : '/',
+    outputDir: 'planet_voyager'
+  
+})
